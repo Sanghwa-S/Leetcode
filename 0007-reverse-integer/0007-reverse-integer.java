1067578b269d66da51e37e02bd0af71c722d1cde
Sanghwa-S <86758391+Sanghwa-S@users.noreply.github.com>
2023-06-09 14:36:46 -0400

    Time: 1 ms (100.00%), Space: 40 MB (60.40%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        int sign = x>0 ? 1:-1;
+        x = Math.abs(x);
+        int mul = (int)Math.log10(x);
+        
+        while(x>0) {
+            int remains = x%10;
+            x= x/10;
+            result += remains*Math.pow(10,mul);
+            
+            if(result > Integer.MAX_VALUE){
+                return 0;
+            }
+            mul--;
+        }
+        
+        return (int)result*sign;
+    }
+}
